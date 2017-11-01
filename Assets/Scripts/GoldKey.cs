c1f4e705653546a233bd519181e711d5b42ead0e
Nicolas St-Onge <stongenicolas@hotmail.com>
2017-11-01 16:10:30 -0400

    add key but not working correctly

---
+++ Assets/Scripts/GoldKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldKey : MonoBehaviour {
+
+    private GameController gameControler;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+
+        gameControler = GameObject.Find("GameController").GetComponent<GameController>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.Play();
+        
+    }
+}
