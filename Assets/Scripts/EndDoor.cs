2b8c340392c14ef3653542b31f3a6cd860cbe387
Nicolas St-Onge <stongenicolas@hotmail.com>
2017-11-02 19:57:05 -0400

    add door mechanism (need key to open end door)

---
+++ Assets/Scripts/EndDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDoor : MonoBehaviour {
+
+    public AudioClip doorLockedSound;
+    public AudioClip doorOpenSound;
+    private AudioSource source;
+
+
+    // Use this for initialization
+    void Start () {
+
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GameObject.Find("Character").GetComponent<Character>().key == true)
+        {
+            GameObject.Find("finish_door").GetComponent<Animation>().Play();
+            source.clip = doorOpenSound;
+            source.Play();
+        }
+
+        else
+        {
+            source.clip = doorLockedSound;
+            source.Play();
+        }
+    }
+}
