9d66e7b8444107c357266139cfbdbca7fee7cd0c
Nicolas St-Onge <stongenicolas@hotmail.com>
2017-11-27 14:34:50 -0500

    add DB fonction

--- Assets/Scripts/DbLoader.cs
+++ Assets/Scripts/DbLoader.cs
@@ -7,14 +7,20 @@ public class DbLoader : MonoBehaviour {
     string addUserUrl = "http://stongenicolas.zapto.org/PWNN/AddUser.php";
     string updateScoreUrl = "http://stongenicolas.zapto.org/PWNN/UpdateScore.php";
     string updateDeathUrl = "http://stongenicolas.zapto.org/PWNN/UpdateDeath.php";
+    string updateLabDoneUrl = "http://stongenicolas.zapto.org/PWNN/UpdateLabDone.php";
     string getAllUsersUrl = "http://stongenicolas.zapto.org/PWNN/GetAllUsers.php";
+    string getUserScoreUrl = "http://stongenicolas.zapto.org/PWNN/GetUserScore.php";
 
     public string[] userList;
+    public int score;
+    public int death;
+    public int labdone;
 
     // Use this for initialization
     IEnumerator Start ()
     {
         yield return GetAllUsers();
+        yield return GetUserScore("Bruno");
     }
 
     public IEnumerator GetAllUsers()
@@ -28,6 +34,17 @@ public class DbLoader : MonoBehaviour {
         {
             userList[i] = dataString.Split(';')[i];
         }  
+    }
+
+    public IEnumerator GetUserScore(string username)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+
+        WWW data = new WWW(getUserScoreUrl, form);
+        yield return data;
+        string dataString = data.text;
+        score = System.Int32.Parse(dataString.Split(';')[0]);
     }
 
     public IEnumerator AddUser(string username)
@@ -59,6 +76,16 @@ public class DbLoader : MonoBehaviour {
         form.AddField("deathPost", nbrDeath);
 
         WWW www = new WWW(updateDeathUrl, form);
+        yield return www;
+    }
+
+    public IEnumerator UpdateLabDone(string username, int nbrLabDone)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("labdonePost", nbrLabDone);
+
+        WWW www = new WWW(updateLabDoneUrl, form);
         yield return www;
     }
 }
